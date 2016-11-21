bfd772199f2844f7003aa110ddca9bf4bdf0ecdf
Yvette Cook <yvettecook@gmail.com>
2016-11-21 09:04:58 +0000

    Add guard to prevent crash on empty array

--- ArrayDiff/FoundationExtensions.swift
+++ ArrayDiff/FoundationExtensions.swift
@@ -24,6 +24,8 @@ public extension IndexSet {
 	deletes in descending order and inserts in ascending order.
 	*/
 	public func indexPathsInSection(_ section: Int, ascending: Bool = true) -> [IndexPath] {
+        guard !self.isEmpty else { return [] }
+
 		var result: [IndexPath] = []
 		result.reserveCapacity(count)
 
